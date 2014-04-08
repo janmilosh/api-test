d062ebc49de53004bc3d78e5b55221997ac117ae
Jan Milosh <janmilosh@gmail.com>
2014-04-08 09:32:35 -0400

    Adds comments and improves organization

--- adjusters.py
+++ adjusters.py
@@ -2,6 +2,7 @@
 from urllib2 import urlopen
 from json import load, dumps
 
+# Get the Mariposa data in JSON format (city, state, and number of adjusters)
 def get_data():
     url = 'https://personnel.mariposaltd.com/api/map/cities'
 
@@ -9,15 +10,21 @@ def get_data():
     json_obj = load(response)
     return json_obj
 
+# Write raw adjuster data to a file called adjuster-data.json
 def print_raw_data(data):
     f = open('adjuster-data.json', 'w')
     f.write(dumps(data, indent=4))
     f.close()
 
-# def convert_json_to_list(json_obj):
-#     for location in json_obj:
-#         print location.city
+# Convert JSON data to a list and sort by number of adjusters (high to low)
+def convert_json_and_sort(json_obj):
+    for location in json_obj:
+        print location.city
 
+# Remove all non-US cities from list and any back-slashes
+def clean_up_city_list:
+
+# Get geodata and add to list
 def get_geodata():
     url = 'http://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx?streetAddress=9355%20Burton%20Way&city=Beverly%20Hills&state=ca&zip=90210&apikey=demo&format=XML&census=false&notStore=false&version=4.01'
     response = urlopen(url)
@@ -26,6 +33,7 @@ def get_geodata():
     xml_data.write(output)
     xml_data.close()
 
+# Create csv file from list containing geodata
 def create_csv_file(data):
     f = open('cities.csv', 'w')
     f.write('city,state,num,lon,lat\n')
@@ -33,6 +41,7 @@ def create_csv_file(data):
         f.write(location['city'].title() + ',' + location['state'] + ',' + str(location['number']) + '\n')
     f.close()
 
+# The main function that makes it all happen
 def main():
     raw_data = get_data()
     print_raw_data(raw_data)
