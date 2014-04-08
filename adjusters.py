f022bff48bcd18b12497b0ce463f0c1b81cb6ffb
Jan Milosh <janmilosh@gmail.com>
2014-04-08 15:17:57 -0400

    Adds geocode functionality for individual cities

--- adjusters.py
+++ adjusters.py
@@ -24,16 +24,20 @@ def convert_json_and_sort(data):
           'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 
           'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 
           'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
-    counter = 0
+    
+    # Create the city list array and get the geodata
     for location in data:
         if location['state'] in states:
-            city_list.append([location['city'].title().decode('string_escape'), location['state'], location['number']])
+            city = location['city'].title().decode('string_escape')
+            state = location['state']
+            location = location['number']
+            city_list.append([city, state, location])
 
-            print('%s,%s,%s,%s' % (counter, city_list[counter][0], city_list[counter][1], city_list[counter][2]))
-            counter += 1
-        else :
-            print('ignored this one: %s,%s,%s' % (location['city'].title(), location['state'], location['number']))
-        
+            # Now that we have the city and state, go get latitude and longitude
+            # This currently just prints xml to the console, but it's a good start
+            get_geodata(city.replace(' ', '%20'), state)
+
+        # Reverse sort the list by number of adjusters    
         city_list.sort(key=lambda x: x[2], reverse=True)
 
         f = open('cities.csv', 'w')
@@ -42,34 +46,23 @@ def convert_json_and_sort(data):
             f.write(item[0] + ',' + item[1] + ',' + str(item[2]) + '\n')
         f.close()
         
-
-# Remove all non-US cities from list and any back-slashes
-#def clean_up_city_list:
-
 # Get geodata and add to list
-def get_geodata():
-    url = 'http://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx?streetAddress=9355%20Burton%20Way&city=Beverly%20Hills&state=ca&zip=90210&apikey=demo&format=XML&census=false&notStore=false&version=4.01'
+def get_geodata(city, state):
+    url = 'http://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx?city=' + city + '&state=' + state + '&apikey=demo&format=XML&census=false&notStore=false&version=4.01'
     response = urlopen(url)
     output = response.read()
-    xml_data = open('xml_response.xml', 'w')
-    xml_data.write(output)
-    xml_data.close()
-
-# Create csv file from list containing geodata
-# def create_csv_file(data):
-#     f = open('cities.csv', 'w')
-#     f.write('city,state,num,lon,lat\n')
-#     for location in data:
-#         f.write(location['city'].title().decode('string_escape') + ',' + location['state'] + ',' + str(location['number']) + '\n')
-#     f.close()
+    print(output)
+    # xml_data = open('xml_response.xml', 'w')
+    # xml_data.write(output)
+    # xml_data.close()
 
 # The main function that makes it all happen
 def main():
     raw_data = get_data()
     convert_json_and_sort(raw_data)
     print_raw_data(raw_data)
-    # create_csv_file(raw_data)
-    get_geodata()
+
+    # get_geodata()
 
 
 if __name__ == '__main__':
