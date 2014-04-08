f8686b21c5513c6b54b84cd31550327db0415bf3
Jan Milosh <janmilosh@gmail.com>
2014-04-07 21:18:25 -0400

    Adds write to xml and csv file capabilities, first test of geocode api

--- adjusters.py
+++ adjusters.py
@@ -14,10 +14,30 @@ def print_raw_data(data):
     f.write(dumps(data, indent=4))
     f.close()
 
-def main():
+# def convert_json_to_list(json_obj):
+#     for location in json_obj:
+#         print location.city
 
+def get_geodata():
+    url = 'http://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx?streetAddress=9355%20Burton%20Way&city=Beverly%20Hills&state=ca&zip=90210&apikey=demo&format=XML&census=false&notStore=false&version=4.01'
+    response = urlopen(url)
+    output = response.read()
+    xml_data = open('xml_response.xml', 'w')
+    xml_data.write(output)
+    xml_data.close()
+
+def create_csv_file(data):
+    f = open('cities.csv', 'w')
+    f.write('city,state,num,lon,lat')
+    for location in data:
+        print location['city'] + ',' + location['state'] + ',' + str(location['number'])
+    f.close()
+
+def main():
     raw_data = get_data()
     print_raw_data(raw_data)
+    create_csv_file(raw_data)
+    get_geodata()
 
 
 if __name__ == '__main__':
